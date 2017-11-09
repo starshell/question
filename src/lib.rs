57146ecb4cdd1b32d5c905858d8adb2636bf7d31
Derek Goddeau <datenstrom@protonmail.com>
2017-11-09 12:25:39 -0500

    Deny all warnings for testing

--- src/lib.rs
+++ src/lib.rs
@@ -13,40 +13,7 @@
 //! ```
 #![cfg_attr(feature = "strict", feature(plugin))]
 #![cfg_attr(feature = "strict", plugin(clippy))]
-#[deny(feature = "strict",
-       bad-style,
-       const-err,
-       dead-code,
-       extra-requirement-in-impl,
-       improper-ctypes,
-       legacy-directory-ownership,
-       non-shorthand-field-patterns,
-       no-mangle-generic-items,
-       overflowing-literals,
-       path-statements,
-       patterns-in-fns-without-body,
-       plugin-as-library,
-       private-in-public,
-       private-no-mangle-fns,
-       private-no-mangle-statics,
-       raw-pointer-derive,
-       safe-extern-statics,
-       unconditional-recursion,
-       unions-with-drop-fields,
-       unused,
-       unused-allocation,
-       unused-comparisons,
-       unused-parens,
-       while-true,
-       missing-debug-implementations,
-       missing-docs,
-       trivial-casts,
-       trivial-numeric-casts,
-       unused-extern-crates,
-       unused-import-braces,
-       unused-qualifications,
-       unused-results)]
-
+#![cfg_attr(feature = "strict", deny(warnings))]
 
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Read, Write};
